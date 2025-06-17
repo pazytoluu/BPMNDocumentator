1e0c27482dec661042ee5e2d2b8f1b3145b70a6b
El Mehdi HALABA <el-mehdi.halaba@atos.net>
2025-06-17 16:03:05 +0100

    first commit

---
+++ src/main/groovy/net/atos/bpmndocumentator/BpmnDocumentatorApplication.groovy
@@ -0,0 +1,13 @@
+package net.atos.bpmndocumentator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BpmnDocumentatorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BpmnDocumentatorApplication, args)
+	}
+
+}
---
+++ src/test/groovy/net/atos/bpmndocumentator/BpmnDocumentatorApplicationTests.groovy
@@ -0,0 +1,13 @@
+package net.atos.bpmndocumentator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BpmnDocumentatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
